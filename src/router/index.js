3632fc91c9069406ce08296d2cd7111f0b29011a
zhaojinxin <zjx@example.com>
2018-05-04 20:18:30 +0800

    项目的页面创建完毕

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,76 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 
+
+//引入tabbar中的组件
+
+import Home from "@/components/tabbar/home";
+import Cart from "@/components/tabbar/cart";
+import Menber from "@/components/tabbar/menber";
+import Search from "@/components/tabbar/search";
+
+// 商品相关的组件
+
+import GoodsList from "@/components/goods/list";
+import GoodDetail from "@/components/goods/detail";
+
+//新闻相关的组件
+
+
+import NewsList from "@/components/news/list";
+import NewDetail from "@/components/news/detail";
+
+// 图片相关的组件
+
+import picList from "@/components/pictures/list";
+import picDetail from "@/components/pictures/detail";
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {
+      path:"/",
+      component:Home
+    },
+    {
+      path:"/menber",
+      component:Menber
+    },
+    {
+      path:"/cart",
+      component:Cart
+    },
+    {
+      path:"/search",
+      component:Search
+    },
+    // 商品的
+    {
+      path:"/goods",
+      component:GoodsList
+    },
+    {
+      path:"/goods/detail",
+      component:GoodDetail
+    },
+    //新闻
+    {
+      path:"/news",
+      component:NewsList
+    },
+    {
+      path:"/news/detail",
+      component:NewDetail
+    },
+    //图片
+    {
+      path:"/pics",
+      component:picList
+    },
+    {
+      path:"/pics",
+      component:picDetail
+    }
   ]
 })
